9ed3f2e7d4765edceb666d9610adef5a2874945f
Patcharin <APIapp APIapp.sln APIapp.userprefs Droid iOS patcharinseetap@gmail.com>
2017-05-06 23:17:25 +0700

    Display pdf from Url,Local,SaveAndDisplay And Pinch to zoom

---
+++ DisplayPDF.WinPhone81/CustomWebViewRenderer.cs
@@ -0,0 +1,24 @@
+﻿using DisplayPDF;
+using DisplayPDF.WinPhone81;
+using System;
+using System.Net;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WinRT;
+
+[assembly: ExportRenderer(typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+namespace DisplayPDF.WinPhone81
+{
+    public class CustomWebViewRenderer : WebViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var customWebView = Element as CustomWebView;
+                Control.Source = new Uri(string.Format("ms-appx-web:///Assets/pdfjs/web/viewer.html?file={0}", string.Format ("ms-appx-web:///Assets/Content/{0}", WebUtility.UrlEncode(customWebView.Uri))));
+            }
+        }
+    }
+}
---
+++ Droid/CustomWebViewRenderer.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using DisplayPDF;
+using DisplayPDF.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer (typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+namespace DisplayPDF.Droid
+{
+	public class CustomWebViewRenderer : WebViewRenderer
+	{
+		protected override void OnElementChanged (ElementChangedEventArgs<WebView> e)
+		{
+			base.OnElementChanged (e);
+
+			if (e.NewElement != null) {
+				var customWebView = Element as CustomWebView;
+				Control.Settings.AllowUniversalAccessFromFileURLs = true;
+
+				//1 From LIB
+				Control.LoadUrl(string.Format("file:///android_asset/pdfjs/web/viewer.html?file={0}",
+												string.Format("file:///android_asset/Content/{0}",
+														   WebUtility.UrlEncode(customWebView.Uri))));
+				//2 Custom Connect with URL for Display PDF
+				//Control.LoadUrl(string.Format("file:///android_asset/pdfjs/web/viewer.html?file={0}",
+				//                              string.Format("{0}",
+				//                             WebUtility.UrlEncode(customWebView.Uri))));
+
+				//3 Save and Display
+				//this.SaveAndDisplay(customWebView.Uri);
+			}
+		}
+		private void SaveAndDisplay(string Uri)
+		{
+			string pdfPath = this.SavePdf("test.pdf", Uri);
+			Control.LoadUrl(string.Format("file:///android_asset/pdfjs/web/viewer.html?file={0}",
+										  string.Format("{0}", WebUtility.UrlEncode(pdfPath))));
+		}
+
+		private string SavePdf(string filename, string link)
+		{
+			WebClient client = new WebClient();
+			string documentsPath = System.IO.Path.Combine((Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDownloads)).Path, filename);
+			client.DownloadFile(link, documentsPath);
+
+			return documentsPath;
+
+		}
+	}
+}
